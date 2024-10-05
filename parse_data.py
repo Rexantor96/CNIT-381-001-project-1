1f46ab0fa6ed8ba4d0722fade884f6e616e2f891
Rexantor96 <willworthin@outlook.com>
2024-10-05 14:15:06 -0500

    wrote json yaml

--- parse_data.py
+++ parse_data.py
@@ -1,13 +1,14 @@
 import xml.etree.ElementTree as tree
 import re
 import json
+import yaml
 
 def parseXML(file, tag, value):
     tag = "{}" + tag
     value = "{}" + value
     xmlFile = tree.parse(file)
     root = xmlFile.getroot()
-    xml = re.match('{.*}', root.tag)
+    xml = re.match('{.*}', root.tag).group(0)
     item = root.find(tag.format(xml))
     result = item.find(value.format(xml))
     return "{}".format(result.text)
@@ -18,8 +19,15 @@ def parseJSON(file, key):
     result = "{}".format(data['name'])
     return result
 
-#print(parseXML("parse.xml", "bk112", "author"))
+def parseYAML(file, key):
+    with open(file, "r") as yamlFile:
+        data = yaml.safe_load(yamlFile)
+    result = "{}".format(data['name'])
+    return result
+
+print(parseXML("parse.xml", "bk112", "author"))
 #print(parseJSON("parse.json", 'Abbrev'))
+#print(parseYAML("parse.yaml", 'Abbrev'))
 
 #https://microsoftedge.github.io/Demos/json-dummy-data/
 #https://learn.microsoft.com/en-us/previous-versions/windows/desktop/ms762271(v=vs.85)
